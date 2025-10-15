54bfceb7d9e43003ad74d0f7169ea38f717bc724
IgniteCoders <theboom.kajas@gmail.com>
2025-10-15 13:59:42 +0200

    We added the Task table with its DAO.

--- app/src/main/java/com/example/todolist_android/data/Task.kt
+++ app/src/main/java/com/example/todolist_android/data/Task.kt
@@ -3,19 +3,23 @@ package com.example.todolist_android.data
 data class Task(
     val id: Int,
     val title: String,
-    val done: Boolean
+    val done: Boolean,
+    val category: Category
 ) {
     companion object {
         const val TABLE_NAME = "Task"
         const val COLUMN_ID = "id"
         const val COLUMN_TITLE = "title"
         const val COLUMN_DONE = "done"
+        const val COLUMN_CATEGORY = "category_id"
 
         const val SQL_CREATE_TABLE =
             "CREATE TABLE $TABLE_NAME (" +
                     "$COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "$COLUMN_TITLE TEXT, " +
-                    "$COLUMN_DONE BOOLEAN)"
+                    "$COLUMN_DONE BOOLEAN," +
+                    "$COLUMN_CATEGORY INTEGER," +
+                    "FOREIGN KEY($COLUMN_CATEGORY) REFERENCES ${Category.TABLE_NAME}(${Category.COLUMN_ID}) ON DELETE CASCADE)"
 
         const val SQL_DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
     }
